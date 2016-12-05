e6ceba7fef403b9a9f6b909bb395e024ebcbf725
Broch Stilley <brochington@gmail.com>
2016-12-05 02:43:13 -0800

    Adding new version of Staction, with some general clean up.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,66 @@
-import declarity from 'declarity';
+import declarity from 'declarity'
+import Staction from 'staction'
 
-const {Entity, register} = declarity;
+const {Entity, register} = declarity
 
-const rectFill = (props, {context}) => {
-    context.fillStyle = "red";
+const processSystems = (props) => {
+    const {systems} = props
+
+    if (Array.isArray(systems) && systems.length > 0) {
+        return systems.reduce((acc, system) => {
+            return typeof system == 'function'
+                       ? system(acc)
+                       : acc
+        }, props)
+    }
+
+    return props
+}
+
+const clearAll = (props) => {
+    props.context.clearRect(0, 0, 100, 100);
+
+    return props;
+}
+
+const fillRect = (props) => {
+    const {context, color, systems} = props
+    context.fillStyle = color
+    context.fillRect(25, 25, 100, 100)
+
+    return props
+}
+
+const addClearBit = (props) => {
+    const {context, systems, count} = props
+
+    context.clearRect(45, 45, 60, 60)
+    context.strokeRect(count, 50, 50, 50)
+
+    return props
 }
 
 class Rect extends Entity {
-    didCreate = ({props}) => {
-        const {context, color} = props;
-
-        context.fillStyle = color;
-        context.fillRect(25, 25, 100, 100);
-        context.clearRect(45, 45, 60, 60);
-        context.strokeRect(50, 50, 50, 50);
+    create = ({props}) => {
+        return processSystems(props)
     }
 
-    update = ({nextProps}) => {
-        console.log('nextProps', nextProps);
-        const {context, count} = nextProps;
+    didCreate = ({props}) => {
 
-        context.fillRect(25, 25, 100, 100);
-        context.clearRect(45, 45, 60, 60);
-        context.strokeRect(count, 50, 50, 50);
+    }
+
+    update = ({props}) => {
+        processSystems(props)
     }
 }
 
 class Canvas extends Entity {
     create = () => {
-        const docRoot = document.getElementById('root');
+        const docRoot = document.getElementById('root')
         const canvas = document.createElement('canvas')
-        const context = canvas.getContext('2d');
+        const context = canvas.getContext('2d')
 
-        docRoot.appendChild(canvas);
+        docRoot.appendChild(canvas)
 
         return {
             docRoot,
@@ -45,35 +73,44 @@ class Canvas extends Entity {
         return [
             <Rect
                 key="rect-1"
-                color="red"
+                color="orange"
                 context={state.context}
                 count={props.count}
-                systems={[]}
+                systems={[clearAll, fillRect, addClearBit]}
             />
-        ];
+        ]
     }
 }
 
 
 class App extends Entity {
-    actions = {
-        updateCount: (state, actions) => {
-            return {count: state().count + 1};
-        }
+    create = ({setState}) => {
+        const initState = {count: 0}
+        this.staction = new Staction();
+
+        this.staction.init(
+            {
+                updateCount: (state, actions) => {
+                    return {count: state().count + 1}
+                }
+            },
+            () => initState,
+            (newState) => {
+                setState(newState)
+            }
+        )
+
+        return initState
     }
 
-    create = () => {
-        return {count: 1}
+    didCreate = ({state}) => {
+        // setInterval(this.staction.actions.updateCount, 1000)
     }
 
-    didCreate = ({actions}) => {
-        actions.updateCount();
-        // setInterval(actions.updateCount, 1000);
-    }
     render = ({state}) => {
         return <Canvas key="rootCanvas" count={state.count}/>
     }
 }
 
 
-register(<App />);
+register(<App />)
